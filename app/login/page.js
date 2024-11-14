b657d2655382138294a6b3c187b5e70bd0fb6173
Timothy Cliff <cliffpermadi08@gmail.com>
2024-11-14 20:08:15 +0700

    initial commit

---
+++ app/login/page.js
@@ -0,0 +1,7 @@
+export default function Login(){
+    return(
+        <div className="flex item-center justify-center h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl font-bold">Login Page</h1>
+        </div>
+    );
+}
