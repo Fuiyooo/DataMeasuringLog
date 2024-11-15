a6c7eb47478a9d324044ff88c840a4fe072b23c6
Prototype2321 <darrell.samuel@student.umn.ac.id>
2024-11-15 17:16:20 +0700

    Login Page update1
    Buat login page sesuai dengan figma

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,35 @@
-export default function Login(){
-    return(
-        <div className="flex item-center justify-center h-screen bg-gray-900 text-white">
-            <h1 className="text-3xl font-bold">Login Page</h1>
+export default function Login() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-[#222E43]">
+        <div className="w-full max-w-sm p-8 space-y-6 bg-[#1B2B3C] rounded-lg shadow-lg">
+          <h2 className="text-3xl font-bold text-center text-white">WELCOME</h2>
+          <p className="text-center text-white">Login to your account</p>
+          <form className="space-y-4">
+            <div>
+              <input
+                type="text"
+                className="w-full px-4 py-2 text-gray-700 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Username"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                className="w-full px-4 py-2 text-gray-700 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 font-bold text-black bg-white rounded-full hover:bg-gray-700"
+            >
+              Login
+            </button>
+          </form>
         </div>
+      </div>
     );
-}
+  }
+  
